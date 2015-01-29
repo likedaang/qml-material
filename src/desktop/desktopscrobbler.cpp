bb0ec45d6fd5f5dfdb2d179ed12c9a80f2b777f7
Bogdan Cuza <boghison22@gmail.com>
2015-01-29 22:05:02 +0200

    Added copyright header

--- src/desktop/desktopscrobbler.cpp
+++ src/desktop/desktopscrobbler.cpp
@@ -1,3 +1,22 @@
+/* 
+ * QML Desktop - Set of tools written in C++ for QML
+ * 
+ * Copyright (C) 2014 Bogdan Cuza <bogdan.cuza@hotmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "desktopscrobbler.h"
 
 DesktopScrobbler::DesktopScrobbler(QQuickItem *parent) : QQuickItem(parent) {
@@ -17,7 +36,6 @@ DesktopScrobbler::DesktopScrobbler(QQuickItem *parent) : QQuickItem(parent) {
     for (int i = 0; i < entries.length(); i++){
         desktopList << new DesktopFile(entries.at(i));
     }
-    //emit desktopFilesChanged(desktopFiles());
     QFileSystemWatcher fileWatcher(entries);
     QFileSystemWatcher *dirWatcher = new QFileSystemWatcher(QStringList() << "/usr/local/share/applications" << "/usr/share/applications" << "~/.local/share/applications");
     connect(&fileWatcher, &QFileSystemWatcher::fileChanged, this, &DesktopScrobbler::processFileModification);
--- src/desktop/desktopscrobbler.h
+++ src/desktop/desktopscrobbler.h
@@ -1,3 +1,22 @@
+/*
+ * QML Desktop - Set of tools written in C++ for QML
+ *
+ * Copyright (C) 2014 Bogdan Cuza <bogdan.cuza@hotmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef DESKTOPSCROBBLER
 #define DESKTOPSCROBBLER
 
@@ -22,7 +41,6 @@ private:
     QFileSystemWatcher *dirWatcher;
     QQuickList<DesktopFile> desktopList;
 private slots:
-    //I wonder if QQuickList handles this stuff automagically, needs testing
     void processFileModification(const QString &path);
     void processDirChange(const QString &path);
 
