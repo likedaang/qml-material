e54d6dfec8d17a0875e8cc869b018604f8b4adb2
Michael Spencer <sonrisesoftware@gmail.com>
2015-01-31 13:53:38 -0600

    Fix a bug in the DesktopFile class

--- src/desktop/desktopfile.cpp
+++ src/desktop/desktopfile.cpp
@@ -1,18 +1,18 @@
-/* 
+/*
  * QML Desktop - Set of tools written in C++ for QML
- * 
+ *
  * Copyright (C) 2014 Bogdan Cuza <bogdan.cuza@hotmail.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -79,8 +79,8 @@ void DesktopFile::processLocation(const QString &location) {
             } else {
                 QStringList sizesList;
                 sizesList << "512x512" << "256x256" << "128x128";
-                for (int i = 0; i < sizesList.length(); i++) {
-                    QString noscalable = envList[i] + "/icons/hicolor/" + sizesList[i] + "/apps/" + tempIcon;
+                for (int j = 0; j < sizesList.length(); j++) {
+                    QString noscalable = envList[i] + "/icons/hicolor/" + sizesList[j] + "/apps/" + tempIcon;
                     if (QFile::exists(noscalable)) {
                         m_icon = noscalable;
                         break;
